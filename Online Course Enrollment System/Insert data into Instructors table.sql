09e6c28f402d1e44875988a281d523bb4c87d2fe
Grigor Jivkov Marinov <grigormarinov761@gmail.com>
2025-07-09 12:25:24 +0300

    Inserted some data into Instructors table

---
+++ Online Course Enrollment System/Insert data into Instructors table.sql
@@ -0,0 +1,12 @@
+USE OnlineCourseEnrollmentSystemDb;
+
+INSERT INTO Instructors (FirstName, LastName, Email, Specialization)
+VALUES 
+    ('John', 'Smith', 'john_smith@gmail.com', 'Software Engineering'),
+    ('Sara', 'Smith', 'sara_smith@gmail.com', 'Data Science'),
+    ('Emily', 'Carter', 'emily.carter92@example.com', 'Digital Marketing'),
+    ('Liam', 'Bennett', 'liam.bennett33@example.io', 'Cybersecurity'),
+    ('Marcus', 'Neal', 'marcus.oneal73@example.net', 'Human Resources'),
+    ('Kevin', 'Zhang', 'kevin.zhang81@example.biz', 'Mechanical Engineering'),     
+    ('Chloe', 'Schmidt', 'c.schmidt19@example.org', 'Machine Learning');
+
